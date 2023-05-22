911fe5b971b6036169556fdc764ee59eca0653fc
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-22 16:23:30 +0530

    Generate JWT Token for Register and Login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -29,13 +29,15 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({ 
     name, 
     email, 
-    password: hashedPassword });
+    password: hashedPassword
+  });
 
   if (user) {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400);
@@ -56,7 +58,8 @@ const loginUser = asyncHandler(async (req, res) => {
     res.status(200).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400);
@@ -71,6 +74,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({message: 'User Data displayed'})
 })
+
+// Generate JSON Web Token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d'
+  })
+}
 
 module.exports = {
   registerUser,
