40ba2c3156d70d7acffcbfcdae5a419cf47be143
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-22 15:59:30 +0530

    Login user controller function complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -48,7 +48,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({message: 'Login User'})
+  const { email, password } = req.body;
+
+  const user = await User.findOne({email});
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(400);
+    throw new Error('Invalid Credentials');
+  }
+
 })
 
 // @desc    Get user data
