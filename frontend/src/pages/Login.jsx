f7af6d26ae94575c03729cab1ecb99609a4ae718
saurabh-kd-singh <sksingh5087@gmail.com>
2023-05-23 17:04:59 +0530

    Login functionality completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { FaSignInAlt } from "react-icons/fa";
-
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from '../components/Spinner';
 
 function Login() {
   // Component Level State
@@ -12,6 +16,33 @@ function Login() {
   // Destructure variables from form data state.
   const { email, password } = formData;
 
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { 
+    user, 
+    isLoading, 
+    isError, 
+    isSuccess, 
+    message 
+  } = useSelector((state) => state.auth);
+
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch])
+
+
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -21,6 +52,18 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData));
+
+  }
+
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
